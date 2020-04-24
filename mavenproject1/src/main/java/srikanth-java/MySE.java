520a1cc1652a6df8cf65fe4aa3b457deb292f990
Srikanth Gajula <Srikanth.gajula@gmail.com>
2020-04-24 19:13:56 +0530

    maven project

---
+++ mavenproject1/src/main/java/srikanth-java/MySE.java
@@ -0,0 +1,20 @@
+package srikanth-java;
+
+import org.objectweb.petals.component.common.HandlingException;
+import org.objectweb.petals.component.common.se.AbstractServiceEngine;
+import org.objectweb.petals.component.common.util.MessageExchangeWrapper;
+import org.objectweb.petals.tools.jbicommon.descriptor.Extensions;
+
+
+public class mavenproject1 extends AbstractServiceEngine {
+
+	@Override
+	protected boolean onExchange(MessageExchangeWrapper exchangeWrapper, Extensions extensions)
+			throws HandlingException {
+
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}
